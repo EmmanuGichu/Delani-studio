bb284124e42370c712e3b33152bcf621fea837d7
emmanuel.gichu@student.moringaschool.com <emmanuel.gichu@student.moringaschool.com>
2021-09-13 06:48:02 +0300

    modified

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-git
+function bounce(){
+    $("mouse-click-icon")
+    .animate({
+        margintop: "-30px",
+        position: "absolute"
+    }, 500)
+    .animate({
+        margintop:"0",
+        position:"absolute"
+    },{
+        duration:500,
+        complete:bounce
+    });
+
+}
+
+function scrollToaboutus(){
+    const position = $("aboutus.section").positon();
+    console.log(position); 
+    $("html, body").animate({
+        scrollTop:positon.top+"px"
+    }, 500);
+    // scrollTo(0, position.top);
+}
+
+$(document).ready(function(){
+    bounce();
+    $("mouse-click-icon").click(scrollToaboutus);
+     
+});
